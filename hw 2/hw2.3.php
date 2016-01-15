d371a9d98bf03b62b54f2ceee1340e8c99e2e597
Vlad Kovalenko <Koovvaall@mail.ru>
2016-01-15 19:13:41 +0300

    ?

--- hw 2/hw2.3.php
+++ hw 2/hw2.3.php
@@ -1,8 +1,6 @@
-$arr = [1,2,3,8,14,89,45];
+<?php
+   $arr = [1,2,3,8,14,89,45];
     $len = count($arr);
-    $newarr =[];
-    for($i=0; $i<$len; $i++){
-            $newarr[$i]=$arr[$len-$i-1];
-        }
-    var_dump($arr);
-    var_dump($newarr);
+    for($i=$len-1; $i>=0; $i--){
+            echo($arr[$i].' ');
+    }
