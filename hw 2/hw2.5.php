3085db3de8060337e85b3a4206c7fc8b8e8406ca
Vlad Kovalenko <Koovvaall@mail.ru>
2016-01-15 19:29:41 +0300

    +

--- hw 2/hw2.5.php
+++ hw 2/hw2.5.php
@@ -7,19 +7,26 @@
 
     <body>
         <?php
-         $homepage = file_get_contents('https://api.vk.com/method/friends.get?fields=sex,photo_200,city,country&user_id=71991427&v=5.44');
-         //echo($homepage);
+        $qs = http_build_query([
+            'fields' => 'photo_200,status,domain,city,country,sex',
+            'user_id' => '132407942',
+            'v' => '5.44',
+        ]);
+         $homepage = file_get_contents('https://api.vk.com/method/friends.get?'.$qs);
+         echo($homepage);
         $decode = json_decode($homepage, $depth = 2);
-         //var_dump($decode['response']['items']);
+         var_dump($decode['response']['items']);
         ?>
 
         <?php foreach ($decode['response']['items'] as $post): ?>
             <section>
                 <h3><?= $post['first_name'].$post['last_name'] ?></h3>
                 <p> id= <?= $post['id'] ?></p>
+                <?= $post['domain'] ?><br>
                 <p class='preview'>
                     <img src="<?= $post['photo_200'] ?>">
                 </p>
+                <?= $post['status'] ?><br>
                 <p>
                     <?php
                         if($post['sex'] == 2){
